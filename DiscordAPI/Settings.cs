9d92a8bd3e641644e420ddc4743fedaabfc108be
swiney2 <92840962+swiney-2@users.noreply.github.com>
2021-11-10 15:37:59 -0600

    Add files via upload

---
+++ DiscordAPI/Settings.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace DiscordWebhook.NET
+{
+    public class Settings { 
+        public static Uri WebHookURL;
+        public static string Message;
+        public static string Username;
+        public static Uri ProfilePicture;
+    }
+}
